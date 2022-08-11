c3a4d1e004375c496a510b07431e6bb613b1c83a
krup <krupskiy.andrey@gmail.com>
2022-08-11 23:45:20 +0200

    add the view layout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use cursive::views::{DummyView, LinearLayout, TextContent, TextView};
+use cursive::views::{LinearLayout, PaddedView, Panel, TextContent, TextView};
 use env_logger::{builder, Target};
 use log::debug;
 use std::sync::Arc;
@@ -6,33 +6,53 @@ use std::thread::sleep;
 use std::time;
 
 fn main() {
-    let mut builder = builder();
-    builder.target(Target::Stderr).init();
-
-    debug!["hello from main"];
+    log_setup();
 
     let mut siv = cursive::default();
-
     siv.add_global_callback('q', |s| s.quit());
 
-    let tv1 = TextView::new("Hello cursive! Press <q> to quit.");
-    let content_2 = TextContent::new("Counter: ");
-    let tv2 = TextView::new_with_content(content_2.clone());
-    siv.add_layer(
-        LinearLayout::vertical()
-            .child(tv1)
-            .child(DummyView)
-            .child(tv2),
-    );
+    let info_content = TextView::new("Press q to quit");
+    let info_view = prettify_text_view(info_content);
 
-    let content_2 = Arc::new(Box::new(content_2));
-    std::thread::spawn(move || update_text_view(content_2.clone()));
+    let storage_content = TextView::new("Hello from storage view!");
+    let storage_view = prettify_text_view(storage_content);
+
+    let network_content = setup_network_view();
+    let network_view = prettify_text_view(network_content);
+
+    let ram_content = TextView::new("Hello from ram view!");
+    let ram_view = prettify_text_view(ram_content);
+
+    let cpu_content = TextView::new("Hello from cpu view!");
+    let cpu_view = prettify_text_view(cpu_content);
+
+    let info_view = LinearLayout::vertical().child(info_view);
+
+    let top_view = LinearLayout::horizontal()
+        .child(storage_view)
+        .child(network_view);
+    let bottow_view = LinearLayout::horizontal().child(ram_view).child(cpu_view);
+
+    let widgets_view = LinearLayout::vertical().child(top_view).child(bottow_view);
+
+    siv.add_fullscreen_layer(
+        LinearLayout::vertical()
+            .child(info_view)
+            .child(widgets_view),
+    );
 
     siv.set_autorefresh(true);
     siv.run();
 }
 
-fn update_text_view(shared_content: Arc<Box<TextContent>>) {
+fn log_setup() {
+    let mut builder = builder();
+    builder.target(Target::Stderr).init();
+
+    debug!["hello from main"];
+}
+
+fn update_network_view_content(shared_content: Arc<Box<TextContent>>) {
     debug!["hello from update_text_view"];
 
     let mut counter: i32 = 0;
@@ -40,7 +60,25 @@ fn update_text_view(shared_content: Arc<Box<TextContent>>) {
         debug!["Counter: {}", counter];
 
         sleep(time::Duration::from_secs(1));
-        shared_content.set_content(counter.to_string());
+        shared_content.set_content(format!("Network view counter: {}", counter.to_string()));
         counter += 1;
     }
+}
+
+fn setup_network_view() -> cursive::views::TextView {
+    let network_content = TextContent::new("Network view counter: ");
+    let network_view = TextView::new_with_content(network_content.clone());
+
+    let content_2 = Arc::new(Box::new(network_content));
+    std::thread::spawn(move || update_network_view_content(content_2.clone()));
+
+    return network_view;
+}
+
+fn prettify_text_view(text_view: TextView) -> Panel<PaddedView<TextView>> {
+    let view = PaddedView::new(cursive::view::Margins::lrtb(2, 2, 2, 2), text_view);
+    let mut view = Panel::new(view);
+    view.set_title("View title");
+
+    return view;
 }
