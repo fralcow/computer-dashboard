dd917089956e8d8756c23315a75e22408abeeb86
krup <krupskiy.andrey@gmail.com>
2022-08-20 21:53:05 +0200

    add network stats

--- src/views/network.rs
+++ src/views/network.rs
@@ -1,5 +1,7 @@
-use crate::beepboop::BeepBooper;
 use cursive::views::{TextContent, TextView};
+use log::error;
+use procfs::net::DeviceStatus;
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time;
@@ -16,11 +18,68 @@ pub fn setup() -> cursive::views::TextView {
 }
 
 fn update_content(network_content: Arc<Box<TextContent>>) {
-    let base_message = network_content.get_content().source().to_owned();
-    let mut beeper = BeepBooper::new();
+    let mut prev_stats = match procfs::net::dev_status() {
+        Ok(stats) => match Some(stats) {
+            Some(stats) => stats,
+            None => {
+                network_content.set_content(format!("Failed to initialize network view"));
+                return;
+            }
+        },
+        Err(e) => {
+            error!("{}", e);
+            network_content.set_content(format!("Failed to initialize network view"));
+            return;
+        }
+    };
+
+    let mut prev_now = std::time::Instant::now();
 
     loop {
         sleep(time::Duration::from_secs(1));
-        network_content.set_content(format!("{}{}", base_message, beeper.beep()));
+
+        let now = std::time::Instant::now();
+        let dev_stats = procfs::net::dev_status().unwrap();
+
+        network_content.set_content(format!(
+            "{}",
+            get_network_stat(now, prev_now, prev_stats, &dev_stats)
+        ));
+
+        prev_stats = dev_stats;
+        prev_now = now;
     }
+}
+
+fn get_network_stat(
+    now: std::time::Instant,
+    prev_now: std::time::Instant,
+    prev_stats: HashMap<String, DeviceStatus>,
+    dev_stats: &HashMap<String, DeviceStatus>,
+) -> String {
+    // calculate diffs from previous
+    let dt = (now - prev_now).as_millis() as f32 / 1000.0;
+
+    let mut stats: Vec<_> = dev_stats.values().collect();
+    stats.sort_by_key(|s| &s.name);
+    let mut result = format!(
+        "{:>16}: {:<20}               {:<20}\n",
+        "Interface", "bytes recv", "bytes sent"
+    );
+    result.push_str(&format!(
+        "{:>16}  {:<20}               {:<20}\n",
+        "================", "====================", "===================="
+    ));
+    for stat in stats {
+        result.push_str(&format!(
+            "{:>16}: {:<20}  {:>6.1} kbps  {:<20}  {:>6.1} kbps\n",
+            stat.name,
+            stat.recv_bytes,
+            (stat.recv_bytes - prev_stats.get(&stat.name).unwrap().recv_bytes) as f32 / dt / 1000.0,
+            stat.sent_bytes,
+            (stat.sent_bytes - prev_stats.get(&stat.name).unwrap().sent_bytes) as f32 / dt / 1000.0
+        ));
+    }
+
+    result
 }
