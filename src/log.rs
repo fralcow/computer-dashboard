b75eff170c0b7afc8f3d61af477126e4eb963aa1
krup <krupskiy.andrey@gmail.com>
2022-09-11 00:46:22 +0200

    set log level from env var

--- src/log.rs
+++ src/log.rs
@@ -1,4 +1,5 @@
 use chrono;
+use std::env;
 
 pub fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
@@ -11,7 +12,19 @@ pub fn setup_logger() -> Result<(), fern::InitError> {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(|| -> log::LevelFilter {
+            match env::var("LOG_LEVEL")
+                .unwrap_or_else(|_| String::from("error"))
+                .to_lowercase()
+                .as_str()
+            {
+                "trace" => log::LevelFilter::Trace,
+                "debug" => log::LevelFilter::Debug,
+                "info" => log::LevelFilter::Info,
+                "warn" => log::LevelFilter::Warn,
+                _ => log::LevelFilter::Error,
+            }
+        }())
         .level_for("cursive_core", log::LevelFilter::Error)
         .chain(fern::log_file("output.log")?)
         .apply()?;
