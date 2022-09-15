a123d54ee416db82ec364c36275cb41a44c70f27
krup <krupskiy.andrey@gmail.com>
2022-09-15 16:08:28 +0200

    add storage module

--- src/views/storage.rs
+++ src/views/storage.rs
@@ -1,8 +1,13 @@
-use crate::beepboop::BeepBooper;
+use ::log::debug;
 use cursive::views::{TextContent, TextView};
+use fstab::FsTab;
+use nix::sys::statvfs;
+use std::path::Path;
 use std::sync::Arc;
 use std::thread::sleep;
 use std::time;
+
+const MTAB_PATH: &str = "/etc/mtab";
 
 pub fn setup() -> cursive::views::TextView {
     let initial_message = String::from("Storage view: ");
@@ -15,16 +20,6 @@ pub fn setup() -> cursive::views::TextView {
     return storage_view;
 }
 
-fn update_content(storage_content: Arc<Box<TextContent>>) {
-    let msg = storage_content.get_content().source().to_owned();
-    let mut beeper = BeepBooper::new();
-
-    loop {
-        storage_content.set_content(format!("{}{}", msg, beeper.beep()));
-        sleep(time::Duration::from_secs(1));
-    }
-}
-
 #[test]
 fn setup_storage_view() {
     let storage_view = setup();
@@ -33,4 +28,66 @@ fn setup_storage_view() {
         .get_content()
         .source()
         .contains("Storage view:"));
+}
+
+fn update_content(storage_content: Arc<Box<TextContent>>) {
+    loop {
+        let result = get_storage_stats();
+
+        debug!("\n{}", result);
+        storage_content.set_content(result);
+
+        sleep(time::Duration::from_secs(1));
+    }
+}
+
+fn get_storage_stats() -> String {
+    let statvfs_vec: Vec<(String, statvfs::Statvfs)> = FsTab::new(Path::new(MTAB_PATH))
+        .get_entries()
+        .unwrap()
+        .into_iter()
+        .filter(|d| d.fs_spec.contains("/dev/"))
+        .filter(|d| !d.fs_spec.contains("mapper"))
+        .map(|path| {
+            debug!("path.mountpoint: {:?}", path.mountpoint.to_str().unwrap());
+            (
+                path.fs_spec.clone(),
+                statvfs::statvfs(path.mountpoint.to_str().unwrap()).unwrap(),
+            )
+        })
+        .collect();
+
+    let mut result: String = String::from(format!(
+        "{:<15}{:<15}{:<15}{:<15}\n",
+        "name", "total_size", "used", "used %"
+    ));
+
+    for stat in statvfs_vec {
+        let total_size = stat.1.blocks() * stat.1.block_size();
+        let used = total_size - stat.1.blocks_free() * stat.1.block_size();
+
+        let (multiplier, postfix): (u64, &str) = match total_size {
+            total_size if total_size < 10_u64.pow(3) => (1, "B"),
+            total_size if total_size < 10_u64.pow(6) => (10_u64.pow(3), "KB"),
+            total_size if total_size < 10_u64.pow(9) => (10_u64.pow(6), "MB"),
+            _ => (10_u64.pow(9), "GB"),
+        };
+
+        let total_size_scaled = (total_size / multiplier) as f32;
+        let used_scaled = (used / multiplier) as f32;
+
+        result.push_str(
+            format!(
+                "{:<15}{:>6.2}{postfix}{:>12.2}{postfix}{:>13.2}%\n",
+                stat.0,
+                total_size_scaled,
+                used_scaled,
+                used_scaled * 100.0 / total_size_scaled
+            )
+            .as_str(),
+        )
+    }
+
+    debug!("result: {:?}", result);
+    result
 }
